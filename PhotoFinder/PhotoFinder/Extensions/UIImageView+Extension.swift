1a273ee42fa76ca63fc2125028e375d9e7ad0e18
Juan Santiago Martin Balbas <jmbalbas@gmail.com>
2017-12-04 10:42:50 +0100

    PF011 - Added image caching

--- PhotoFinder/PhotoFinder/Extensions/UIImageView+Extension.swift
+++ PhotoFinder/PhotoFinder/Extensions/UIImageView+Extension.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+let imageCache = NSCache<AnyObject, AnyObject>()
+
 extension UIImageView {
     
     func loadImageUrl(_ urlString: String?, placeholder: UIImage?) {
@@ -17,23 +19,33 @@ extension UIImageView {
         
         guard let urlString = urlString, let url = URL(string: urlString) else { return }
         
+        // Check if the image is already cached
+        if let image = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
+            self.image = image
+            return
+        }
+        
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let httpURLResponse = response as? HTTPURLResponse else { return }
             
             // Check status code
             if httpURLResponse.statusCode != 200 {
-                print("Error downloading image: \(urlString)\ncode: \(httpURLResponse.statusCode)")
+                print("Error downloading image: \(urlString).\nCode: \(httpURLResponse.statusCode)")
                 return
             }
             
             // Check error
             if let error = error {
-                print("Error downloading image: \(urlString)\nerror: \(error.localizedDescription)")
+                print("Error downloading image: \(urlString).\nError: \(error.localizedDescription)")
                 return
             }
             
             if let data = data,
                 let image = UIImage(data: data) {
+                
+                // Save image in cache
+                imageCache.setObject(image, forKey: urlString as AnyObject)
+                
                 DispatchQueue.main.async() { [weak self] in
                     // Update the UI
                     self?.image = image
